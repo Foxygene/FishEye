b1fce9aee07d1480eb90409ca1d004d80fe744a5
Foxygene <foxygene.dev@gmail.com>
2022-07-16 01:49:33 +0200

    feat(contact modal): done

--- scripts/lib/photographer/contactForm.js
+++ scripts/lib/photographer/contactForm.js
@@ -1,30 +1,16 @@
 export const displayModal = (element) => {
   const modal = document.querySelector('.modal');
 
-  const sendBtn = document.querySelector('#send_btn');
+  const form = document.querySelector('.contact_form');
 
   const photographerName = document.createElement('h2');
   photographerName.textContent = element.name;
   photographerName.classList.add('header_text');
   modal.insertBefore(photographerName, modal.children[1]);
 
-  const inputsConsoleLogs = () => {
-    const firstName = document.querySelector('#first_name');
-    console.log(firstName);
-
-    const lastName = document.querySelector('#last_name');
-    console.log(lastName);
-
-    const email = document.querySelector('#email');
-    console.log(email);
-
-    const yourMessage = document.querySelector('#your_message');
-    console.log(yourMessage);
-
-    sendBtn.addEventListener('submit', (event) => {
-      event.preventDefault();
-      console.log('twerk');
-      inputsConsoleLogs();
-    });
-  };
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const { firstName, lastName, email, yourMessage } = event.currentTarget.elements;
+    console.log(firstName.value, lastName.value, email.value, yourMessage.value);
+  });
 };
