37748ab64bc35a8f8c1ff4c872289e176f42f382
Foxygene <foxygene.dev@gmail.com>
2022-07-15 22:30:50 +0200

    feat(contact modal): WIP handling inputs into console

--- scripts/lib/photographer/contactForm.js
+++ scripts/lib/photographer/contactForm.js
@@ -1,8 +1,30 @@
 export const displayModal = (element) => {
   const modal = document.querySelector('.modal');
 
+  const sendBtn = document.querySelector('#send_btn');
+
   const photographerName = document.createElement('h2');
   photographerName.textContent = element.name;
   photographerName.classList.add('header_text');
   modal.insertBefore(photographerName, modal.children[1]);
+
+  const inputsConsoleLogs = () => {
+    const firstName = document.querySelector('#first_name');
+    console.log(firstName);
+
+    const lastName = document.querySelector('#last_name');
+    console.log(lastName);
+
+    const email = document.querySelector('#email');
+    console.log(email);
+
+    const yourMessage = document.querySelector('#your_message');
+    console.log(yourMessage);
+
+    sendBtn.addEventListener('submit', (event) => {
+      event.preventDefault();
+      console.log('twerk');
+      inputsConsoleLogs();
+    });
+  };
 };
