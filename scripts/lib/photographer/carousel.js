58c88e8712067494567fdd7e8714e3b7f96ac283
Foxygene <foxygene.dev@gmail.com>
2022-07-18 22:20:14 +0200

    feat(carousel): WIP js file base

--- scripts/lib/photographer/carousel.js
+++ scripts/lib/photographer/carousel.js
@@ -1,3 +1,24 @@
 export const getCarouselDOM = () => {
-  console.log('hello');
+  const carousel = document.createElement('div');
+  carousel.classList.add('carousel');
+
+  const leftArrow = document.createElement('div');
+  leftArrow.classList.add('left_arrow');
+  carousel.appendChild(leftArrow);
+
+  const mediaBox = document.createElement('div');
+  mediaBox.classList.add('media_box');
+  carousel.appendChild(mediaBox);
+
+  const media = document.createElement('img');
+  mediaBox.appendChild(media);
+
+  const mediaTitle = document.createElement('p');
+  mediaBox.appendChild(mediaTitle);
+
+  const rightArrow = document.createElement('div');
+  carousel.appendChild(rightArrow);
+
+  const close = document.createElement('div');
+  carousel.appendChild(close);
 };
