96aa2edf7f43f50dedcdbfe844aaba5c267c01aa
Foxygene <foxygene.dev@gmail.com>
2022-07-25 17:16:56 +0200

    feat(carousel): added base button layout

--- scripts/lib/photographer/carousel.js
+++ scripts/lib/photographer/carousel.js
@@ -2,7 +2,9 @@ export const getCarouselDOM = (media, id) => {
   const carousel = document.createElement('div');
   carousel.classList.add('carousel');
 
-  const leftArrow = document.createElement('button');
+  const leftArrow = document.createElement('img');
+  leftArrow.classList.add('left_arrow');
+  leftArrow.setAttribute('src', 'assets/icons/right-arrow.png');
   carousel.appendChild(leftArrow);
 
   const mediaBox = document.createElement('div');
@@ -24,15 +26,19 @@ export const getCarouselDOM = (media, id) => {
   const mediaTitle = document.createElement('p');
   mediaBox.appendChild(mediaTitle);
 
-  const rightArrow = document.createElement('button');
-  carousel.appendChild(rightArrow);
-
-  const close = document.createElement('div');
+  const close = document.createElement('img');
+  close.classList.add('carousel_close_btn');
+  close.setAttribute('src', 'assets/icons/close-black.svg');
+  close.addEventListener('click', () => {
+    const carousel = document.querySelector('.carousel');
+    carousel.remove();
+  });
   carousel.appendChild(close);
 
-  const closeIcon = document.createElement('img');
-  close.setAttribute('src', 'assets/icons/close.svg');
-  close.appendChild(closeIcon);
+  const rightArrow = document.createElement('img');
+  rightArrow.classList.add('right_arrow');
+  rightArrow.setAttribute('src', 'assets/icons/right-arrow.png');
+  carousel.appendChild(rightArrow);
 
   document.querySelector('main').append(carousel);
 };
